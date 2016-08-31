c0f47d265edbe03c25a6036f45159c6b80cd73d5
Nikita Vasilyev <me@elv1s.ru>
2016-08-30 17:18:37 -0700

    Reduce indentation.

---
+++ debugger-popover-indentation/index.js
@@ -0,0 +1,19 @@
+var MyApp = {
+    foo: {
+        bar: {
+            buz: function(a) {
+                var x = 42;
+                return a + x;
+            }
+        }
+    }
+};
+
+
+
+
+
+
+
+
+MyApp.foo.bar.buz(0);
