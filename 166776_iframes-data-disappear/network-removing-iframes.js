d795068a1a13666db87d885989e30f78e8cfb240
Nikita Vasilyev <me@elv1s.ru>
2017-01-09 13:24:57 -0800

    https://bugs.webkit.org/show_bug.cgi?id=166776

--- 166776_iframes-data-disappear/network-removing-iframes.js
+++ 166776_iframes-data-disappear/network-removing-iframes.js
@@ -1,9 +1,10 @@
 window.onload = function() {
     var iframe = document.createElement('iframe');
+    window._iframe = iframe;
     iframe.src = "iframe-1.html";
     document.body.appendChild(iframe);
 
-    setTimeout(function() {
-        iframe.remove();
-    }, 1000);
+    //setTimeout(function() {
+    //    iframe.remove();
+    //}, 1000);
 };
